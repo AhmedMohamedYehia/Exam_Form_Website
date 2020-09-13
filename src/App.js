95923c6e4373dddbe3512d7e9b49f0ee719f349f
Mostafa-99 <59765480+Mostafa-99@users.noreply.github.com>
2020-09-13 16:50:58 +0200

    Timer component added but need some styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import DisplayTime from './Components/DisplayTime';
+import "antd/dist/antd.css";
 
 function App() {
+  const [time, setTime]= useState({seconds:0, minutes:40})
+  const [interv, setInterv] = useState();
+
+  var updatedSeconds=time.seconds;
+  var updatedMinutes=time.minutes;
+
+  useEffect(()=>{
+      start();
+  },[]);
+  const start = () => {
+    run();
+    setInterv(setInterval(run, 1000));
+  };
+
+  const run = () => {
+    if(updatedSeconds === 0 && updatedMinutes !== 0){
+      updatedMinutes--;
+      updatedSeconds = 60;
+    }
+    if(updatedMinutes === 0 && updatedSeconds ===0){}
+    else{
+      updatedSeconds--;
+    }
+    return setTime({seconds:updatedSeconds, minutes:updatedMinutes});
+  };
+
   return (
     <div className="App">
-      start
+      <DisplayTime time={time}/>
     </div>
   );
 }
---
+++ src/Components/DisplayTime.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './DisplayTime.css'
+import { Progress, Row } from 'antd';
+
+function DisplayTime(props) {
+    const totalNumberOfMinutes=40;
+    var percentage=(((props.time.minutes*60)+props.time.seconds)/(totalNumberOfMinutes*60))*100;
+  return (
+    <Row justify="center">
+      <div id="display-time">
+        <div className="display-time-container">
+            <div>
+                  <span>{(props.time.minutes >= 10)? props.time.minutes : "0"+ props.time.minutes}</span>&nbsp;:&nbsp;
+                  <span>{(props.time.seconds >= 10)? props.time.seconds : "0"+ props.time.seconds}</span>
+            </div>
+            <Progress percent={percentage} />
+          </div> 
+      </div>
+    </Row>
+  );
+}
+
+export default DisplayTime;
